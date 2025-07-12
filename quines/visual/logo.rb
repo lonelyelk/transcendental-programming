45d1999f18e6795d01ae471ac6349934f43059a1
Sergey Kruk <sergey.kruk@gmail.com>
2025-07-12 22:57:29 +0200

    Make logo file size smaller

--- quines/visual/logo.rb
+++ quines/visual/logo.rb
@@ -1,27 +1,25 @@
-eval(q=%w!w=62;h=27;d=200;c=40;r=24;pi=3.14;s=32.chr;g=33.chr;
-e=27.chr;q="eval(q=%w#{g}#{q}#{g}.join)".chars;u=->(ph){k=d*2;
-k.times.each_with_object({}){|j,o|x=r*Math.cos(pi*4*j/k+ph);y=
-(j<k/32||j>=31*k/                             32)&&0.31-0.24*M
-ath.cos(pi*8*    j/k);y||=     j     >=k/32&&j    <15*k/32&&0.
-42*Math.si   n(pi*4*j     /k);y||=j>=     15*k/32&   &j<17*k/3
-2&&0.24*M  ath.cos(   pi*8*j/k)-0.31;y||=   -0.42*Ma  th.sin(p
-i*4*j/k)  ;y*=r;   o[[x.round,y.round]]=true   ;}};b=  u[pi/10
-];n=0;a  =h.ti   mes.map{|y|w.times.map{|x|t=b   [[x-w  /2,y-h
-/2]];t& &n+=  1;t&&q.sample||(q[y*w+x-n]||?#)}*''  };z= ->(x,y
-,m){m&& "#{  e}[30;44m#{s}#{e}[0m"||s};o=->(f){h.t  ime s{|y|w
-.times{ |x  |print(z[x,y,f[[x-w/2,y-h/2]]])};puts};     sleep(
-0.07);p   rint("#{e}[#{h}A")};l=->(fr,ph,s=true){k=d*fr;k.time
-s.each_w ith_object({}){|j,o|x=r*Math.cos(pi*2*j/k*fr+ph);y=r*
-0.42*Ma   th.sin(pi*2*j/k);o[[x.round,y.round]]=true;o[[-x.rou
-nd,y.ro un  d]]||=s;}};c.times{|i|o[l[1,3*pi/2*(1-M     ath.co
-s(pi*i/ c/2  ))]]};z=->(x,y,m){m&&"#{e}[34m#{a[y][  x]} #{e}[0
-m"||s}; c.ti  mes{|i|o[l[2,pi/2+2.828*pi*Math.sin (-pi/ 4+pi*i
-/c/2)**  3,fal   se]]};z=->(x,y,m){m&&s||a[y][   x]};c  .times
-{|i|o[l[  3,pi/3   +pi*Math.sin(pi*i/c),fals   e]]};z  =->(x,y
+eval(q=%w!include(Math);w=62;h=25;d=200;c=40;r=24;pi=3.14;s=32
+.chr;g=33.chr;e=27.chr;q="eval(q=%w#{g}#{q}#{g}.join)".chars;u
+=->(ph){k=d*2;k.t                             imes.inject({}){
+|o,j|x=r*cos(    pi*4*j/k+     p     h);y=(j<k    /32||j>=31*k
+/32)&&0.31   -0.24*co     s(pi*8*j/k)     ;y||=j>=   k/32&&j<1
+5*k/32&&0  .42*sin(   pi*4*j/k);y||=j>=15   *k/32&&j  <17*k/32
+&&0.24*c  os(pi*   8*j/k)-0.31;y||=-0.42*sin   (pi*4*  j/k);y*
+=r;o[[x  .roun   d,y.round]]=true;o}};b=u[pi/1   0];n=  0;a=h.
+times.m ap{|  y|w.times.map{|x|t=b[[x-w/2,y-h/2]]  ;t&& n+=1;t
+&&q.sam ple  ||(q[y*w+x-n]||?#)}*''};z=->(x,y,m){m  &&" #{e}[3
+0;44m#{ s}  #{e}[0m"||s};o=->(f){h.times{|y|w.times     {|x|pr
+int(z[x   ,y,f[[x-w/2,y-h/2]]])};puts};sleep(0.07);print("#{e}
+[#{h}A") };l=->(fr,ph,s=true){k=d*fr;k.times.inject({}){|o,j|x
+=(r*cos   (pi*2*j/k*fr+ph)).round;y=(r*0.42*sin(pi*2*j/k)).rou
+nd;o[[x ,y  ]]=true;o[[-x,y]]||=s;o}};c.times{|i|o[     l[1,3*
+pi/2*(1 -co  s(pi*i/c/2))]]};z=->(x,y,m){m&&"#{e}[  34m #{a[y]
+[x]}#{e }[0m  "||s};c.times{|i|o[l[2,pi/2+2.828*p i*sin (-pi/4
++pi*i/c  /2)**   3,false]]};z=->(x,y,m){m&&s||   a[y][  x]};c.
+times{|i  |o[l[3   ,pi/3+pi*sin(pi*i/c),fals   e]]};z  =->(x,y
 ,m){m&&"#  {e}[30;4   4m#{a[y][x]}#{e}[0m   "||a[y][  x]};c.ti
-mes{|i|o[l   [1,pi/2+     2.828*pi*Mat    h.sin(-p   i/4+pi*i/
-c/2)**3]]};c=    20;z=->(x     ,      y,m){m&&    "#{e}[30;44m
-#{s+e}[0m"||a[y][                              x]};c.times{|i|
-o[u[pi/2-4*pi*Math.sin(pi*i/c/2)/10]]};c.times{o[b]};z=->(x,y,
-m){m&&s||a[y][x]};o[b];print("#{e}[#{h}B")##lonelyelk##2025###
-##cresta##!.join)#############################################
+mes{|i|o[l   [1,pi/2+     2.828*pi*sin    (-pi/4+p   i*i/c/2)*
+*3]]};c=20;z=    ->(x,y,m)     {      m&&"#{e}    [30;44m#{s+e
+}[0m"||a[y][x]};c                              .times{|i|o[u[p
+i/2-4*pi*sin(pi*i/c/2)/10]]};c.times{o[b]};z=->(x,y,m){m&&s||a
+[y][x]};o[b];print("#{e}[#{h}B")#lonelyelk#2025#cresta#!.join)
--- quines/visual/logo_generator.rb
+++ quines/visual/logo_generator.rb
@@ -1,6 +1,7 @@
-# eval(q=%w!
+eval(q=%w!
+include(Math);
 w = 62;
-h = 27;
+h = 25;
 d = 200;
 c = 40;
 r = 24;
@@ -9,38 +10,38 @@ s=32.chr;
 g=33.chr;
 e=27.chr;
 q="eval(q=%w#{g}#{q}#{g}.join)".chars;
-u = ->(ph){k=d*2;k.times.each_with_object({}){|j, o|
-  x = r*Math.cos(pi*4*j/k+ ph);
-  y = (j<k/32||j>=31*k/32)&&0.31 - 0.24*Math.cos(pi*8*j/k);
-  y||=j>=k/32&&j<15*k/32&&0.42 * Math.sin(pi*4*j/k);
-  y||=j>=15*k/32&&j<17*k/32&&0.24 * Math.cos(pi*8*j/k) - 0.31;
-  y||=-0.42 * Math.sin(pi*4*j/k);
+u = ->(ph){k=d*2;k.times.inject({}){|o,j|
+  x = r*cos(pi*4*j/k+ ph);
+  y = (j<k/32||j>=31*k/32)&&0.31 - 0.24*cos(pi*8*j/k);
+  y||=j>=k/32&&j<15*k/32&&0.42 * sin(pi*4*j/k);
+  y||=j>=15*k/32&&j<17*k/32&&0.24 * cos(pi*8*j/k) - 0.31;
+  y||=-0.42 * sin(pi*4*j/k);
   y *= r;
-  o[[x.round,y.round]] = true;
+  o[[x.round,y.round]] = true;o
 }};
 b = u[pi/10];
 n=0;
 a = h.times.map{|y|w.times.map{|x|t=b[[x-w/2,y-h/2]];t&&n+=1;t&&q.sample||(q[y*w+x-n]||?#)}*''};
 z=->(x,y,m){m&&"#{e}[30;44m#{s}#{e}[0m"||s};
 o = ->(f){h.times{|y| w.times{|x| print(z[x,y,f[[x-w/2,y-h/2]]])};puts};sleep(0.07);print("#{e}[#{h}A")};
-l = ->(fr,ph,s=true){k=d*fr;k.times.each_with_object({}){|j, o|
-  x = r*Math.cos(pi*2*j/k * fr + ph);
-  y = r*0.42*Math.sin(pi*2*j/k);
-  o[[x.round,y.round]] = true;
-  o[[-x.round,y.round]] ||= s;
+l = ->(fr,ph,s=true){k=d*fr;k.times.inject({}){|o,j|
+  x = (r*cos(pi*2*j/k * fr + ph)).round;
+  y = (r*0.42*sin(pi*2*j/k)).round;
+  o[[x,y]] = true;
+  o[[-x,y]] ||= s;o
 }};
-c.times{|i|o[l[1, 3*pi/2*(1 - Math.cos(pi*i/c/2))]]};
+c.times{|i|o[l[1, 3*pi/2*(1 - cos(pi*i/c/2))]]};
 z=->(x,y,m){m&&"#{e}[34m#{a[y][x]}#{e}[0m"||s};
-c.times{|i|o[l[2, pi/2 + 2.828*pi*Math.sin(-pi/4 + pi*i/c/2)**3,false]]};
+c.times{|i|o[l[2, pi/2 + 2.828*pi*sin(-pi/4 + pi*i/c/2)**3,false]]};
 z=->(x,y,m){m&&s||a[y][x]};
-c.times{|i|o[l[3, pi/3 + pi*Math.sin(pi*i/c),false]]};
+c.times{|i|o[l[3, pi/3 + pi*sin(pi*i/c),false]]};
 z=->(x,y,m){m&&"#{e}[30;44m#{a[y][x]}#{e}[0m"||a[y][x]};
-c.times{|i|o[l[1, pi/2 + 2.828*pi*Math.sin(-pi/4 + pi*i/c/2)**3]]};
+c.times{|i|o[l[1, pi/2 + 2.828*pi*sin(-pi/4 + pi*i/c/2)**3]]};
 c = 20;
 z=->(x,y,m){m&&"#{e}[30;44m#{s+e}[0m"||a[y][x]};
-c.times{|i|o[u[pi/2 - 4*pi*Math.sin(pi*i/c/2)/10]]};
+c.times{|i|o[u[pi/2 - 4*pi*sin(pi*i/c/2)/10]]};
 c.times{o[b]};
 z=->(x,y,m){m&&s||a[y][x]};
 o[b];
 print("#{e}[#{h}B")
-##lonelyelk##2025#####cresta##!.join)
+#lonelyelk#2025#cresta#!.join)
